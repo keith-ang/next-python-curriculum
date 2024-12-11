bf1626df119215eaf494b0a529b624ea9e28bc80
keith <keithangjy96@gmail.com>
2024-12-11 15:31:38 +0700

    fix eslint issue

--- src/components/ChatbotRAG.tsx
+++ src/components/ChatbotRAG.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { useChat } from "ai/react";
 import { Message } from "ai";
 import Bubble from './Bubble';
