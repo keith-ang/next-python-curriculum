8c94463669e1150026c86076e48ccbbeebb93b77
keith <keithangjy96@gmail.com>
2024-12-11 15:31:31 +0700

    fix webpage title

--- src/pages/[filename].js
+++ src/pages/[filename].js
@@ -1,16 +1,20 @@
 import React from 'react';
+import Head from 'next/head';
 import { useRouter } from 'next/router';
 import CurriculumPage from '../components/CurriculumPage';
 import Navbar from '../components/Navbar';
 import { useSelector } from 'react-redux';
 
-const FilenamePage = () => {
+const FilenamePage = ({ data }) => {
   const router = useRouter();
   const { filename } = router.query;
   const days = useSelector((state) => state.days);
 
   return (
     <>
+      <Head>
+        <title>{data.title}</title>
+      </Head>
       <Navbar />
       <CurriculumPage days={days} filename={filename} />
     </>
@@ -21,6 +25,7 @@ export async function getServerSideProps(context) {
   const { filename } = context.params;
   const response = await fetch(`${process.env.NEXT_PUBLIC_FRONTEND_URL}/api/day/${filename}`);
   const data = await response.json();
+  console.log(data)
 
   return {
     props: { data },
